29eb34b30cb69d2b57a87b7578397b2d856bdfeb
Roshini1099 <65917489+Roshini1099@users.noreply.github.com>
2021-01-08 02:49:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {React,Component} from 'react';
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import {Navigationbar} from './components/Navigationbar';
+import Booking from './components/Booking';
+import Admin from './components/Admin';
+
+import "./App.css";
+class App extends Component{
+  render()
+  {
+    return(
+      <div className="App">
+        <Navigationbar/>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/booking" component={Booking}/>
+            <Route exact path="/admin" component={Admin}/>
+          </Switch>  
+       </BrowserRouter>
+      </div>
+      
+    );
+  }
+
+
+}
+
+export default App;
